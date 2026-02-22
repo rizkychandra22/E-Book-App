ca60c2cd92d201da0b7df787a18f64dfffa77139
rizkychandra <rizkychandra2204@gmail.com>
2026-02-22 12:28:33 +0700

    First Commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,8 @@
+<script setup>
+defineProps({ title: String })
+</script>
+
+<template>
+  <h1 class="text-2xl font-bold">{{ title }}</h1>
+  <p>Selamat datang di proyek E-Book!</p>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'title' => 'E-Book App Laravel + Vue'
+    ]);
+});
