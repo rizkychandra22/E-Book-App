acaf02496e78da0780ee1795c8c1b2e3dac76183
rizkychandra <rizkychandra2204@gmail.com>
2026-02-22 14:59:46 +0700

    Page Login & Dashboard to Layouts

---
+++ resources/js/Layouts/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <div id="app" class="d-flex">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">E-Book App</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <Link class="nav-link" href="/dashboard">Dashboard</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" href="/books">Table Buku</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" href="/loans">Rekap Peminjaman Buku</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" href="/users">Users</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container-fluid mt-5 pt-3">
+            <div class="row">
+                <div class="col-md-12">
+                    <slot />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+import App from '../Layouts/App.vue'
+
+defineOptions({ layout: App })
+// Script untuk dashboard logic
+</script>
+
+<template>
+    <Head title="Dashboard - E-Book App" />
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Dashboard</h3>
+                    </div>
+                    <div class="card-body">
+                        <p>Welcome to the E-Book App Dashboard!</p>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="card text-white bg-primary mb-3">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Total Books</h5>
+                                        <p class="card-text">100</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="card text-white bg-success mb-3">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Users</h5>
+                                        <p class="card-text">50</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="card text-white bg-warning mb-3">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Categories</h5>
+                                        <p class="card-text">10</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
