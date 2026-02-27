33d31f211ccd1c6d1003035b7f023cd185bd84d7
rizkychandra <rizkychandra2204@gmail.com>
2026-02-27 21:33:18 +0700

    install tailwindcss

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "tailwindcss";
 
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
